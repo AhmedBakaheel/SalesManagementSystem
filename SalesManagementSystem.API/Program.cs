da560c4e56dd30f9a08e9ac8ca0266cf85325c07
AhmedBakaheel <ahmedomerbakaheel@gmail.com>
2025-12-01 08:43:31 +0300

    Fluent Validation_Done...

--- SalesManagementSystem.API/Program.cs
+++ SalesManagementSystem.API/Program.cs
@@ -3,8 +3,9 @@ using SalesManagementSystem.Infrastructure.Data;
 using SalesManagementSystem.Infrastructure.Repositories;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection; 
-
+using System.Reflection;
+using FluentValidation;
+using SalesManagementSystem.Application.Pipelines;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
@@ -17,7 +18,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddMediatR(typeof(IUnitOfWork).Assembly);
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.Services.AddValidatorsFromAssembly(typeof(IUnitOfWork).Assembly);
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ SalesManagementSystem.Application/Features/Customers/Queries/Commands/CreateCustomerCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using SalesManagementSystem.Application.DTOs.Customers;
+
+namespace SalesManagementSystem.Application.Features.Customers.Commands
+{
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} cannot be empty.") 
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+
+            RuleFor(p => p.Phone)
+                .NotEmpty().WithMessage("{PropertyName} cannot be empty.")
+                .NotNull();
+            RuleFor(p => p.CreditLimit)
+                .GreaterThanOrEqualTo(0).WithMessage("{PropertyName} must be zero or greater.");
+        }
+    }
+}
