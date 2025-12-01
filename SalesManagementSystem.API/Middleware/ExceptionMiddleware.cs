d0dc88619d8106c36ec3a0b0998f4d526046b880
AhmedBakaheel <ahmedomerbakaheel@gmail.com>
2025-12-01 09:02:07 +0300

    Exception Middleware...

---
+++ SalesManagementSystem.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using System.Net;
+using System.Text.Json;
+
+namespace SalesManagementSystem.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            string message = "Internal Server Error.";
+            string errors = null;
+
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    message = "Validation Failed.";
+                    errors = JsonSerializer.Serialize(validationException.Errors.Select(e => new { e.PropertyName, e.ErrorMessage }));
+                    break;
+
+                default:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            context.Response.StatusCode = (int)statusCode;
+
+            var result = JsonSerializer.Serialize(new
+            {
+                message = message,
+                errors = errors ?? exception.Message
+            });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
--- SalesManagementSystem.API/Program.cs
+++ SalesManagementSystem.API/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using FluentValidation;
 using SalesManagementSystem.Application.Pipelines;
+using SalesManagementSystem.API.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
@@ -20,12 +21,13 @@ builder.Services.AddMediatR(typeof(IUnitOfWork).Assembly);
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddValidatorsFromAssembly(typeof(IUnitOfWork).Assembly);
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
